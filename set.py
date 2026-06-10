f1ccf674a94af9700afde3f4fbd11bbd27ca3fe3
piuu09 <priyanavghade@gmail.com>
2026-06-10 22:23:35 +0530

    Update set.py

--- set.py
+++ set.py
@@ -1,6 +1,8 @@
 # set
 x=set()
 print(type(x))
+
+# add element in a set
 x.add(30)
 print(x)
 
@@ -8,15 +10,18 @@ print(x)
 x={10,20,30,40,10}
 print(x)
 
-
+# set-->list
 xl=list(x)
 print(xl[0])
 
 xl[2]=90
 print(xl)
+
+# list-->set
 x=set(xl)
 print(x,type(x))
 
+# different opration on set
 A={1,2,3}
 B={3,4,1}
 print(A.union(B))
