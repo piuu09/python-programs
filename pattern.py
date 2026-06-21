36b6cfe93cc9f91d018a7c9a555be92776d44a8c
piuu09 <priyanavghade@gmail.com>
2026-06-21 21:24:11 +0530

    Add files via upload

---
+++ pattern.py
@@ -0,0 +1,38 @@
+for i in range (4):
+    for j in range(4):
+        print("*",end="")
+    print()
+
+#---------------------------------------------------------
+for i in range(3):
+    for j in range(3):
+        if (i+j)%2==0:
+            print("x",end=" ")
+        else:
+            print("o",end="")
+    print()
+
+#--------------------------------------------------------
+for i in range(3):
+    num=2*i+1
+    
+    for j in range(i+1):
+        print(num,end="")
+    
+ 
+    print()        
+
+# --------------------------------------------------------
+for i in range(3):
+    print(3,end="")
+print()
+for i in range(5):
+    print(5,end="")
+print()
+for i in range(3):
+    print(3,end="")
+print()
+print(1)
+
+#-----------------------------------------------------------
+
